c5da2cb7e2d7aba5e1d94fa8f073571d055d9ab9
alisaitbilgi <alisaitbilgi@gmail.com>
2018-07-05 00:00:05 +0300

    Create infra for github badge

--- src/components/body/content/firstTab/firstTab.js
+++ src/components/body/content/firstTab/firstTab.js
@@ -1,18 +1,42 @@
-// import axios from 'axios'
+import axios from 'axios'
 import $store from '../../../../store/index.js'
+import {converter} from 'number-gilder'
 
 export default function init () {
   return {
     name: 'FirstTab',
-    mounted () {
-      const data = {a: 5, b: 7}
-
-      $store.commit('SET_USER_DATA', data)
-    },
     computed: {
       userData: function () {
         return $store.state.userData
       }
+    },
+    methods: {
+      setUserInput (event) {
+        if (event.target.value) {
+          this.getUserData(event.target.value)
+        }
+        event.target.value = ''
+      },
+      getUserData (username) {
+        axios
+          .get(`https://api.github.com/users/${username}`)
+          .then(val => {
+            const dataTbWritten = {
+              name: val.data.name || val.data.login,
+              followers: converter(val.data.followers),
+              following: converter(val.data.following),
+              repos: val.data.public_repos,
+              gists: val.data.public_gists,
+              avatar: val.data.avatar_url
+            }
+
+            $store.commit('SET_USER_DATA', dataTbWritten)
+          })
+          .catch(() => $store.commit('SET_USER_DATA', {name: 'No User Found'}))
+      }
+    },
+    mounted () {
+      // this.getUserData('nzakas')
     }
   }
 }
